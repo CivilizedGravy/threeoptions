ad7b70f18e6a2cf564514dc25b4fc8217c74a41c
earthonion <garlic@duck.com>
2023-03-21 23:47:52 -0400

    added login and signup

--- application/__init__.py
+++ application/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 import random,string
 app = Flask(__name__)
 app.secret_key = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
-#from . import route
+from . import route
