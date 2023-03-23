eb2c4be72da496a990e695eccb1cb8ae0d396b07
earthonion <garlic@duck.com>
2023-03-22 21:20:17 -0400

    fixed flask_login not working in production

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
+import os
 import random,string
 app = Flask(__name__)
-app.secret_key = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
+app.secret_key = b'\xbd\rzl\x86dK#19\xa8_\xa4\xf9\xb2\xa2\xa1X\xd3\x01\xdc\x0f\tP'
 from . import route
